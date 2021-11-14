2d96a7e90a89e5ff504b4b2c39179a438304098e
Milad Nakhlkar <milad.nakhlkar@gmail.com>
2021-11-14 17:00:27 +0100

    Updating Sitename

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
-          <Link to="/">cryptomil</Link>
+          <Link to="/">CryptoMil</Link>
         </Typography.Title>
         <Button
           className="menu-control-container"
